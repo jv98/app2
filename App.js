21577228ee40c4939ab51a448bd3b8b09c72bbad
jv98 <jacob@vahlen.se>
2021-02-16 15:12:38 +0100

    skapa en knapap so säger hello

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image,StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import logo from './bilder/bärg.jpg';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+    let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+  }
   return (
     <View style={styles.container}>
-      <Text>Hello johan</Text>
-      <Text> ge mig dit kriditkort</Text>
-      <StatusBar style="auto" />
+      <Image source={logo} style={styles.logo}/>
+
+      <Text  style={styles.instructions}> för att dela ett foto från din telffone med en vän klicka på knappen under </Text>
+      <TouchableOpacity onPress={(openImagePickerAsync) => alert('Hello, world!')} style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
+     
     </View>
   );
 }
@@ -19,4 +36,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 305,
+    height: 250,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 15,
+    textAlign: 'center',
+    marginHorizontal: 25,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  }, 
 });
