39def13b187504de64fb7edc9c393e232af0611d
jv98 <jacob@vahlen.se>
2021-02-16 19:11:47 +0100

    test för bilder

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { Image,StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import logo from './bilder/bärg.jpg';
 import * as ImagePicker from 'expo-image-picker';
 
+
 export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState(null);
     let openImagePickerAsync = async () => {
     let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
@@ -15,13 +17,28 @@ export default function App() {
 
     let pickerResult = await ImagePicker.launchImageLibraryAsync();
     console.log(pickerResult);
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  }
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+    );
   }
   return (
     <View style={styles.container}>
       <Image source={logo} style={styles.logo}/>
 
-      <Text  style={styles.instructions}> för att dela ett foto från din telffone med en vän klicka på knappen under </Text>
-      <TouchableOpacity onPress={(openImagePickerAsync) => alert('Hello, world!')} style={styles.button}>
+      <Text  style={styles.instructions}> för att dela ett foto från din telefon med en vän klicka på knappen under </Text>
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
         <Text style={styles.buttonText}>Pick a photo</Text>
       </TouchableOpacity>
      
@@ -51,9 +68,11 @@ const styles = StyleSheet.create({
     backgroundColor: "blue",
     padding: 20,
     borderRadius: 5,
+    marginTop: 200,
   },
   buttonText: {
     fontSize: 20,
     color: '#fff',
+    
   }, 
 });
